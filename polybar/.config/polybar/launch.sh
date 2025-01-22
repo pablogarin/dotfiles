19dca2d45182ad56637844e32dadc13aef99a674
pablogarin <pablo.garin@hotmail.com>
2025-01-22 15:58:13 -0800

    added rofi and polybar

---
+++ polybar/.config/polybar/launch.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Terminate already running bar instances
+# If all your bars have ipc enabled, you can use 
+polybar-msg cmd quit
+# Otherwise you can use the nuclear option:
+# killall -q polybar
+
+# Launch bar1 and bar2
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    echo "---" | tee -a "/tmp/polybar$m.log"
+    MONITOR=$m polybar --reload bar 2>&1 | tee -a "/tmp/polybar$m.log" & disown
+  done
+else
+  echo "---" | tee -a /tmp/polybar1.log
+  polybar --reload bar 2>&1 | tee -a /tmp/polybar1.log & disown
+fi
